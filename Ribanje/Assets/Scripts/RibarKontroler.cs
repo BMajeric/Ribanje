0992d52deca4aadabf73d0b3c676c3d45838270f
fransaganic <fran.saganic@fer.hr>
2023-11-04 21:37:59 +0100

    Ribar novi attackovi, prijelazi iz animacija jos se rade

--- Ribanje/Assets/Scripts/RibarKontroler.cs
+++ Ribanje/Assets/Scripts/RibarKontroler.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using JetBrains.Annotations;
 using Unity.Mathematics;
 using Unity.VisualScripting;
 using UnityEngine;
@@ -14,6 +15,8 @@ public class NewBehaviourScript : MonoBehaviour
     Vector3 playerOrijentacija;
 
     public float brzinaKretanja;
+
+    bool canMove = true;
     
     // Start is called before the first frame update
     void Start()
@@ -24,24 +27,12 @@ public class NewBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        horizontal = Input.GetAxisRaw("Horizontal");
-        vertical = Input.GetAxisRaw("Vertical"); 
-        playerOrijentacija = transform.localScale;
 
-        if (horizontal < 0)
-            playerOrijentacija.x = math.abs(transform.localScale.x); 
-        if (horizontal > 0)
-            playerOrijentacija.x = math.abs(transform.localScale.x) * -1;
+        checkAttack();
 
-        transform.localScale = playerOrijentacija;
-        animator.SetFloat("Run", Mathf.Abs(horizontal));
-        animator.SetFloat("RunUp", vertical);
-
-        if (Input.GetKeyDown(KeyCode.X))
-        animator.SetBool("Attack", true);
-
-        if (Input.GetKeyUp(KeyCode.X))
-        animator.SetBool("Attack", false);
+        if(canMove){
+            checkMovement();
+        }
 
     }
 
@@ -49,5 +40,36 @@ public class NewBehaviourScript : MonoBehaviour
         
         body.velocity = new Vector2(horizontal * brzinaKretanja, vertical * brzinaKretanja);
 
+    }
+
+    void checkAttack(){
+
+        if (Input.GetKeyDown(KeyCode.X)){
+            animator.SetBool("Attack", true);
+            canMove = false;
+            horizontal = 0;
+            vertical = 0;
+        }
+        
+        if (Input.GetKeyUp(KeyCode.X)){
+            animator.SetBool("Attack", false);
+            canMove = true;
+        }
+    }
+
+    void checkMovement(){
+
+        horizontal = Input.GetAxisRaw("Horizontal");
+            vertical = Input.GetAxisRaw("Vertical"); 
+            playerOrijentacija = transform.localScale;
+
+            if (horizontal < 0)
+                playerOrijentacija.x = math.abs(transform.localScale.x); 
+            if (horizontal > 0)
+                playerOrijentacija.x = math.abs(transform.localScale.x) * -1;
+
+            transform.localScale = playerOrijentacija;
+            animator.SetFloat("Run", Mathf.Abs(horizontal));
+            animator.SetFloat("RunUp", vertical);
     }
 }
